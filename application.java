ed5402ba9d04b493d46e05df3d50dd45d8f126ce
nannapanenisaitej <saitejanannapaneni@gmail.com>
2024-06-22 14:04:29 +0530

    new changes for version2.1.2

--- application.java
+++ application.java
@@ -18,3 +18,16 @@ public class Main {
       System.out.println(num + " is not a prime number.");
   }
 }
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 29;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
